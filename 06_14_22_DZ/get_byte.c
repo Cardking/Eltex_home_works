f3b41f19a21df7ef0f8cd6ef24df0eb8e6c5b47c
Cardking <galil.m4a1.ak47@yadnex.ru>
2022-06-19 13:47:03 +0700

    06_14_22_DZ: Сomments added.

--- 06_14_22_DZ/get_byte.c
+++ 06_14_22_DZ/get_byte.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-#define REG 0xDDCCBBAA
+#define REG 0xDDCCBBAA  // Register value
 
 int main()
 {
 	int i;
-	for(i = 0; i < 4; ++i){
+	for(i = 0; i < 4; ++i){ // Output each byte.
 		printf("0x%08X\n", (REG >> 8*i)& 0xFF);
 	}
-	printf("0x%08X\n",(REG & ~(0xFF << 8*2)) | (0xEE << 8*2));
+	printf("0x%08X\n\n",(REG & ~(0xFF << 8*2)) | (0xEE << 8*2)); // Ñhange third byte and output register.
+	
 	return 0;
 }
